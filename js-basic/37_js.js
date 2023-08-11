334f5bdf8900f52db8551256b1b780fc288e93b9
adibfuego90 <adiburraman11@gmail.com>
2023-08-11 12:36:44 +0600

    basic-37:String Literal vs Constructor

---
+++ js-basic/37_js.js
@@ -0,0 +1,12 @@
+let str = "My name is String Literal";
+console.log(str);
+
+/* Method -1 */
+let a = 10;
+let str2 = a.toString(a);
+console.log(typeof str2);
+
+/* Method -2  */
+let b = 23;
+let str3 = String(b);
+console.log(str3);
