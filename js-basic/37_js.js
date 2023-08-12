2d90a8b5fd09ef78cf05e98ce5af2b7abc67f23f
adibfuego90 <adiburraman11@gmail.com>
2023-08-12 13:12:50 +0600

    37_improve

--- js-basic/37_js.js
+++ js-basic/37_js.js
@@ -1,5 +1,11 @@
+/* String Literal:
+
+A string literal is a sequence of characters enclosed in double quotes. When you create a string using a literal, the compiler creates a string object in the memory pool called the "string pool" or "string intern pool." If a string with the same content already exists in the pool, the new string reference points to the existing object. */
 let str = "My name is String Literal";
 console.log(str);
+
+/* String Constructor:
+You can create strings using the String class constructor as well. This approach creates a new string object regardless of whether a string with the same content already exists in the pool. This can be useful when you want to ensure that a new object is created, rather than reusing an existing one. */
 
 /* Method -1 */
 let a = 10;
