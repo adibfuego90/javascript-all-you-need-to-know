ac50027ae5b3c5d6f8b30ee89c9eb3e55de5da76
adibfuego90 <adiburraman11@gmail.com>
2023-08-24 14:16:56 +0600

    Update type coercion

--- practice/type-coercion.js
+++ practice/type-coercion.js
@@ -1,3 +1,4 @@
+/* Type coercion in JavaScript refers to the automatic conversion of one data type to another when operations or comparisons involving different data types occur. JavaScript tries to make sense of the expressions by converting values to a common data type before performing the operation. This can lead to unexpected behaviors if not understood properly. */
 /* let num = 10;
 let str = "10";
 let result = num + str;
