859d11452a6fd8d283825bdb6d5aecd05ee4088b
adibfuego90 <adiburraman11@gmail.com>
2023-10-03 19:52:14 +0600

    JS Object litral vs Object Constructor

---
+++ js-basic/52.js
@@ -0,0 +1,21 @@
+// Object literal and Object Constructor
+
+// Literal way :
+let obj = {
+  x: 32,
+  "is sale": true, //note that if name has two separate word then we have to use "" this;
+};
+
+obj.y = 322; // assign to the obj object ;
+obj.x = 90; //Update x ;
+
+// Constructor way :
+
+let arr2 = Object();
+arr2.x = 90;
+arr2.z = 89;
+
+// 2nd way of constructor :
+let obj3 = new Object();
+arr3.c = true;
+arr3.d = false;
