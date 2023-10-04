fce94f5f981d6dc287221cbddde24b08df9dcf09
adibfuego90 <adiburraman11@gmail.com>
2023-10-04 06:34:25 +0600

    Setting object properties in js

---
+++ js-basic/54.js
@@ -0,0 +1,16 @@
+// Setting object properties in js
+
+let point = {
+  x: 10,
+  y: 20,
+  z: 30,
+};
+
+// Dot notation :
+point.x = 1000;
+console.log(point);
+
+// Array notation :
+let prop = "z";
+point[prop] = 3000;
+console.log(point);
