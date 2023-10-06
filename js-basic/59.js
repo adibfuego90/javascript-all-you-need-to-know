a07ecdd9e29cb9e07e1e0278aaa695bf8034aae2
adibfuego90 <adiburraman11@gmail.com>
2023-10-06 19:15:36 +0600

    Function in javascript

---
+++ js-basic/59.js
@@ -0,0 +1,5 @@
+// Function in javascript
+
+/* 1. In general function has 3 part -> 1.input 2.process 3.output
+2.A function is basic level abstraction .
+3.Repeated work can be handle with function. In a function we have full control. */
