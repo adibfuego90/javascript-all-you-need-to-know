2fc01f7f6fea35db84e1ea68239bf2bbde94a233
adibfuego90 <adiburraman11@gmail.com>
2023-10-06 19:54:47 +0600

    Invoking a function in js

---
+++ js-basic/61.js
@@ -0,0 +1,11 @@
+// Invoking a function js
+
+function add() {
+  console.log("I am a function");
+}
+add(); // Invoke add() function
+
+const simple = function () {
+  console.log("I am also a function");
+};
+simple();
