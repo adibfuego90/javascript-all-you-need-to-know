8b022b51a8262318a43d473554c9ecc19403ce0f
adibfuego90 <adiburraman11@gmail.com>
2023-10-12 09:37:45 +0600

    Update on function scope

--- js-basic/67.js
+++ js-basic/67.js
@@ -11,3 +11,21 @@ function x() {
   y();
 }
 x();
+
+// var can be accessed outside block
+
+if (true) {
+  var b = 90;
+  console.log(b); //we can access "b" from any where
+}
+
+console.log(b);
+
+// let and const is block scope not function scope
+if (true) {
+  oiy;
+  let letVar = "I am let variable";
+  const constVar = "I am const variable";
+}
+// console.log(letVar);  //error
+// console.log(constVar); //error
