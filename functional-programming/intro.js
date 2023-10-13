74998b2093bac40224848a48ae6e1fb4629ce85b
adibfuego90 <adiburraman11@gmail.com>
2023-10-13 09:36:46 +0600

    Functional Programming

---
+++ functional-programming/intro.js
@@ -0,0 +1,8 @@
+// functional programming is a programming paradigm a style of building the structure and elements of computer program that treats computation as the evaluation of mathematical function and avoids changing state and mutable data.  -> wikipedia
+
+/* // Three Main Terms 
+  1.Pure Function 
+  2.First Class Function 
+  3.Higher Order Function  */
+
+// Function Acts like a value
