6b14b4a2f45c46d74fd3c37cc129bb4d9576ac9c
adibfuego90 <adiburraman11@gmail.com>
2023-10-14 10:43:56 +0600

    First Class Function in js

---
+++ functional-programming/first-class.js
@@ -0,0 +1,32 @@
+/* 1.A function can be stored in  a variable
+2.A function can be stored in an array
+3.A function can be stored in an object
+4.We can create function as need
+5.We can pass Function as a arguments
+6.We can return function from another function */
+
+function add(a, b) {
+  return a + b;
+}
+
+// A function can be stored in  a variable
+let sum = add;
+// console.log(sum(12, 23));
+
+// A function can be stored in an array
+let arr = [];
+arr.push(add);
+// console.log(arr);
+// console.log(arr[0](21, 233));
+
+// A function can be stored in an object
+let obj = {
+  sum: add,
+};
+// console.log(obj);
+// console.log(obj.sum(1, 2));
+
+// We can create function as need
+setTimeout(function () {
+  console.log("I am a function");
+}, 1000);
