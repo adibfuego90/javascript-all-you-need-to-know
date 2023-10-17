22f0a60f7a03e8f8883d996938a02975200f158b
adibfuego90 <adiburraman11@gmail.com>
2023-10-17 21:37:49 +0600

    Update on map in js

--- functional-programming/map.js
+++ functional-programming/map.js
@@ -6,7 +6,7 @@ let arr = [1, 2, 4, 5, 12];
 
 console.log(sqrArr); */
 
-function myMap(arr) {
+/* function myMap(arr) {
   let newArray = [];
   for (let i = 0; i < arr.length; i++) {
     let temp = arr[i] * arr[i];
@@ -15,3 +15,18 @@ function myMap(arr) {
   return newArray;
 }
 console.log(myMap(arr));
+ */
+
+function myMap2(arr, cb) {
+  let newArray2 = [];
+  for (let i = 0; i < arr.length; i++) {
+    let temp = cb(arr[i], i, arr);
+    newArray2.push(temp);
+  }
+  return newArray2;
+}
+console.log(
+  myMap2(arr, function (value) {
+    return value * value * value;
+  })
+);
