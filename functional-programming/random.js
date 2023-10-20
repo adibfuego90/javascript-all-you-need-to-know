36edfbde4b009e63f8ff626cd27e74cd1391f142
adibfuego90 <adiburraman11@gmail.com>
2023-10-20 14:29:09 +0600

    Random for practice

---
+++ functional-programming/random.js
@@ -0,0 +1,28 @@
+//Sort method
+/* let arr = [-1, 0, -23, 23, 11, 34, 45, 56];
+
+let result = arr.sort(function (a, b) {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  } else {
+    return 0;
+  }
+});
+console.log(result);
+ */
+
+// Every method
+/* let arr = [12, 3, 3, 1, 5, 6];
+let result = arr.every(function (value) {
+  return value >= 0;
+});
+console.log(result); */
+
+// some method
+/* let arr = [23, 34, 45, 56, 5768, 7, 7];
+let result = arr.some(function (value) {
+  return value % 2 === 0;
+});
+console.log(result); */
