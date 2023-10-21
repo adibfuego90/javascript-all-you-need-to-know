53d62d611a4fb2d92026a59945587587e5065206
adibfuego90 <adiburraman11@gmail.com>
2023-10-21 16:01:51 +0600

    Random-2

--- functional-programming/random.js
+++ functional-programming/random.js
@@ -26,3 +26,27 @@ let result = arr.some(function (value) {
   return value % 2 === 0;
 });
 console.log(result); */
+
+/* 
+In Functional programming there is 3 main terms : 1. Pure Function 2.First class function 3. Higher Order function 
+*/
+
+/* 
+// Pure Function:
+function add(a, b) {
+  return a + b;
+}
+let result = add(23, 34);
+console.log(result);
+ */
+
+/* 
+// Not Pure Function :
+let a = 90;
+function rn() {
+  a = 900;
+}
+rn();
+console.log(a); //Value of variable has changed so this function is not a pure function */
+
+// First Class Function :
