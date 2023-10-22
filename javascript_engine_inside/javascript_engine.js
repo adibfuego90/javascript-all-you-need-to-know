f3e767da62a30bfa08e67ad93f5884d25846e2d4
adibfuego90 <adiburraman11@gmail.com>
2023-10-22 09:01:26 +0600

    Chapter-10 : JS Engine insider

---
+++ javascript_engine_inside/javascript_engine.js
@@ -0,0 +1,8 @@
+/* 
+Is a another program which can rad,understand and translate javascript code.
+
+engines : v8, spider monkey, chakra, Javascript core;
+
+source code -> parser -> AST(Abstraction window tree) -> Machine code -> Execution 
+
+*/
