91d09fd7e67006a69928b7997f7e43eba9398a4e
adibfuego90 <adiburraman11@gmail.com>
2023-10-24 19:47:43 +0600

    Chapter-10: Random

--- javascript_engine_inside/random.js
+++ javascript_engine_inside/random.js
@@ -8,14 +8,14 @@ function outer() {
 outer();
 // console.log(a); //We can't access a just because
 
-// Print number 1 t0 100
+// Sum number 1 t0 100
 /* let result = 0;
 for (let i = 0; i <= 100; i++) {
   result += i;
 }
 console.log("result : " + result); */
 
-// Using recursive function
+/* // Using recursive function
 function add(n) {
   if (n === 0) {
     return 0;
@@ -23,4 +23,20 @@ function add(n) {
   return n + add(n - 1);
 }
 let result = add(100);
+console.log(result); */
+
+// Add odd number less than 100
+/* let result = 0;
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 1) {
+    result += i;
+  }
+}
+console.log(result); */
+
+// Other way of do that
+let result = 0;
+for (let i = 1; i <= 100; i += 2) {
+  result += i;
+}
 console.log(result);
