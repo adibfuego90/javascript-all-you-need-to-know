f0479ca716a54eb944fb770ab8c9ac74507e3ac0
adibfuego90 <adiburraman11@gmail.com>
2023-10-24 17:11:17 +0600

    Chapter-10: random.js

--- javascript_engine_inside/random.js
+++ javascript_engine_inside/random.js
@@ -6,4 +6,21 @@ function outer() {
   inner();
 }
 outer();
-console.log(a); //We can't access a just because
+// console.log(a); //We can't access a just because
+
+// Print number 1 t0 100
+/* let result = 0;
+for (let i = 0; i <= 100; i++) {
+  result += i;
+}
+console.log("result : " + result); */
+
+// Using recursive function
+function add(n) {
+  if (n === 0) {
+    return 0;
+  }
+  return n + add(n - 1);
+}
+let result = add(100);
+console.log(result);
