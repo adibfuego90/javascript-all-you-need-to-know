5df28888b66a87738d7f96a6bb2a13941d31bb81
adibfuego90 <adiburraman11@gmail.com>
2023-11-12 15:17:08 +0600

    closure pt-2

--- javascript_engine_inside/closure.js
+++ javascript_engine_inside/closure.js
@@ -1,6 +1,6 @@
 /* Closure is when a function is able to remember and access it's lexical scope even when that function executing outside it's lexical scope  */
 
-// Within scope
+/* // Within scope
 
 function test() {
   let msg = "I am Adibur rahman";
@@ -10,4 +10,17 @@ function test() {
   sayHi();
 }
 
-test();
+test(); */
+
+// Outside lexical scope
+
+function test() {
+  let msg = "I am Adibur Rahman";
+
+  return function () {
+    console.log(msg);
+  };
+}
+
+let sayHi = test();
+sayHi();
