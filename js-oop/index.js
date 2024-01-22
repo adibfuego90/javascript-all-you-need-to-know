8e0dadae54b4e328862a5cc865170d73d6990bd0
adibfuego90 <adiburraman11@gmail.com>
2024-01-22 11:51:18 +0600

    this keyword in js

--- js-oop/index.js
+++ js-oop/index.js
@@ -1,8 +1,23 @@
-let obj = {
+/* let obj = {
   width: 90,
   height: 10,
   area: function () {
     return this.width * this.height;
   },
 };
-console.log(obj.area());
+console.log(obj.area()); */
+
+/* // Understand "this" keyword
+// 1. when you use this keyword into obj then it ref that obj
+let obj = {
+  draw: function () {
+    console.log(this);
+  },
+};
+obj.draw();
+
+// 2. use this without any object it will ref window object
+function myFunc() {
+  console.log(this);
+}
+myFunc(); */
